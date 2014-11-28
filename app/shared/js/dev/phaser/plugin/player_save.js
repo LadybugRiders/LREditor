375d3ba2df15eff78242e137224db85fa9492d4e
unknown <pandavigoureux29@gmail.com>
2014-11-28 15:35:12 +0100

    player save

--- app/shared/js/dev/phaser/plugin/player_save.js
+++ app/shared/js/dev/phaser/plugin/player_save.js
@@ -11,7 +11,9 @@
 */
 Phaser.Plugin.PlayerSave = function( _game, _parent ){
 	this.saveData = {levels:{}};
+	this.activeLevelName = null;
 	this.game = _game;
+	this.storageSave = null;
 
 	createSave(this);
 
@@ -50,6 +52,19 @@ Phaser.Plugin.PlayerSave.prototype.getValue = function(_key){
 }
 
 /**
+* Write the single data into localStorage. The value written will be the temporary one.
+* Be aware that arrays and objects are not supported by local storage. You need to stringify them
+* @param key Name of the variable
+* @method writeValue
+*/
+Phaser.Plugin.PlayerSave.prototype.writeValue = function(_key){	
+  	if( this.tempData[_key] ){
+  		this.saveData[_key] = this.tempData[_key];
+  		this.storedSave[_key] = this.saveData[_key];
+  	}
+}
+
+/**
 * Write the save.
 *
 * @method writeSave
@@ -58,17 +73,26 @@ Phaser.Plugin.PlayerSave.prototype.writeSave = function(){
 	//write Save
 	this.saveData = JSON.parse(JSON.stringify(this.tempData));
 	this.tempData = JSON.parse(JSON.stringify(this.saveData));
+
   	localStorage.setItem("game_save",JSON.stringify(this.saveData));
 }
 
 /**
 * Returns the current save for the actual game.
 * This actually returns the temporary save. Use writeSave to apply changes to the overall game save
-*
+* Use getPersistentSave to access persistent data 
 * @method getSave
 */
 Phaser.Plugin.PlayerSave.prototype.getSave = function(){
 	return this.tempData;
+}
+
+/**
+* Returns the current persistent save. This data is unchanged until you apply (write) the temporary save
+* @method getPermanentSave
+*/
+Phaser.Plugin.PlayerSave.prototype.getPermanentSave = function(){
+	return this.saveData;
 }
 
 /**
@@ -98,6 +122,29 @@ Phaser.Plugin.PlayerSave.prototype.getLevelSave = function(_levelName){
 }
 
 /**
+* Returns the save previously activated for the specified level
+*
+* @method getActiveLevelSave
+* @return {Object}
+*/
+Phaser.Plugin.PlayerSave.prototype.getActiveLevelSave = function(){
+	return this.getLevelSave(this.activeLevelName);
+}
+
+/**
+* Activate a level save. By doing this, you may get the current active level save using getActiveLevelSave.
+* This doesn't affect the save whatsoever, it's just a tools for a quick access 
+*( if you have a level with many sublevels for example, you can gather all the data in only one level save)
+*
+* @method activateLevelSave
+* @param levelName Name of the level in the save 
+* @return {Object}
+*/
+Phaser.Plugin.PlayerSave.prototype.activateLevelSave = function(_levelName){
+	this.activeLevelName = _levelName;
+}
+
+/**
 * Revert the save for the current level
 *
 * @method revertLevelSave
@@ -107,12 +154,25 @@ Phaser.Plugin.PlayerSave.prototype.revertLevelSave = function(){
 	this.tempData[levelName] = JSON.parse(JSON.stringify(this.saveData[levelName]));
 }
 
+/**
+* Delete save. This deletes even the stored save of the game.
+*
+* @method deleteSave
+*/
+Phaser.Plugin.PlayerSave.prototype.deleteSave = function(){
+	this.saveData = {levels:{}};
+	this.tempData = {levels:{}};
+	localStorage.setItem("game_save",JSON.stringify(this.saveData));
+}
+
 function createSave(_this){	
   	var storedSave = localStorage.getItem("game_save");
-  	console.log(storedSave);
+
   	if( storedSave == null){
   		localStorage.setItem("game_save",JSON.stringify(_this.saveData));
   	}
-  	_this.loadSave = JSON.parse(storedSave);
-  	_this.tempData = JSON.parse(storedSave);
+  	//storageSave contains save data as a string
+  	_this.storageSave = storedSave;
+  	_this.saveData = JSON.parse(storedSave);
+  	_this.tempData = JSON.parse(storedSave);;
 }
